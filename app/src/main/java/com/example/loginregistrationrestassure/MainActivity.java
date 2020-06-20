e71e74b11cff64bdb3758e4255e42133455bc2d1
khairun12 <nowrinzafor@gmail.com>
2020-06-20 18:37:58 +0600

    Gradle update

---
+++ app/src/main/java/com/example/loginregistrationrestassure/MainActivity.java
@@ -0,0 +1,14 @@
+package com.example.loginregistrationrestassure;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
